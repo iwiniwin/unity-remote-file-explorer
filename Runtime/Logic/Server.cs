5bae2c38b516589e94cdc5dfa056878a73ae9791
iwiniwin <iwiniwin@foxmail.com>
2021-11-09 10:32:41 +0800

    优化大文件传输效率

--- Runtime/Logic/Server.cs
+++ Runtime/Logic/Server.cs
@@ -40,6 +40,8 @@ namespace RemoteFileExplorer
             Status = ConnectStatus.Connecting;
             m_Server.BeginAcceptTcpClient((asyncResult) => {
                 m_CurrentClient = m_Server.EndAcceptTcpClient(asyncResult);
+                m_CurrentClient.SendBufferSize = defaultBufferSize;
+                m_CurrentClient.ReceiveBufferSize = defaultBufferSize;
                 StartTransferThreads();
             }, this);
         }
